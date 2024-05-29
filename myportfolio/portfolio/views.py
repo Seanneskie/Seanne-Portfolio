02481451fb0ea65b4759442224894ac5d65de2b4
Seanneskie <seannecanete32@gmail.com>
2024-05-29 22:27:18 +0800

    initialcommit

--- myportfolio/portfolio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-def project_list(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio/project_list.html', {'projects': projects})
---
+++ static/index.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.nav-link');
+
+    for (const link of links) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - 70, // Adjust 70 for navbar height
+                behavior: 'smooth'
+            });
+        });
+    }
+
+    const sections = document.querySelectorAll('section');
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const sectionObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        sectionObserver.observe(section);
+    });
+});
