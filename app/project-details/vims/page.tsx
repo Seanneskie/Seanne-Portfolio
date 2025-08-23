038ab6a50a4d39ae8ad50b90a77b1c9791336196
Seanneskie <91924632+Seanneskie@users.noreply.github.com>
2025-08-23 08:55:29 +0800

    feat: add Vims project details component

--- app/project-details/vims/page.tsx
+++ app/project-details/vims/page.tsx
@@ -1,9 +1,10 @@
-import ProjectHtml from "@/components/project-details/ProjectHtml";
+import Vims from "@/components/project-details/Vims";
 
 export default async function Page() {
   return (
     <main className="container mx-auto max-w-5xl px-4 py-12">
-      <ProjectHtml slug="vims" />
+      <Vims />
     </main>
   );
 }
+
