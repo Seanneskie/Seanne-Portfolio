78da10501b1198f19c7365d9719e454ecd63a6ac
Seanneskie <91924632+Seanneskie@users.noreply.github.com>
2025-08-23 15:40:42 +0800

    refactor project detail routing

--- app/project-details/example-project/page.tsx
+++
@@ -1,9 +0,0 @@
-import ExampleProject from "@/components/project-details/ExampleProject";
-
-export default function Page() {
-  return (
-    <main className="container mx-auto max-w-5xl px-4 py-12">
-      <ExampleProject />
-    </main>
-  );
-}
